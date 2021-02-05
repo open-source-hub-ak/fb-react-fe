ba057bb21b75b4a5f06d21d3a6225ca7849e99f4
aakash <aakash>
2021-02-05 17:35:18 +0530

    StoryReel Component added

---
+++ src/StoryReel.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Story from "./Story";
+import "./StoryReel.css";
+function StoryReel() {
+  return (
+    <div className="storyReel">
+      <Story
+        image="https://th.bing.com/th/id/OIP.aJ1uZzv6RwE-ywKw_o8wtwHaEo?pid=Api&rs=1"
+        title="Vivek"
+        profileSrc="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+      />
+      <Story
+        image="https://th.bing.com/th/id/OIP.aJ1uZzv6RwE-ywKw_o8wtwHaEo?pid=Api&rs=1"
+        title="Mayur"
+        profileSrc="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+      />
+      <Story
+        image="https://th.bing.com/th/id/OIP.aJ1uZzv6RwE-ywKw_o8wtwHaEo?pid=Api&rs=1"
+        title="Jay"
+        profileSrc="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+      />
+      <Story
+        image="https://th.bing.com/th/id/OIP.aJ1uZzv6RwE-ywKw_o8wtwHaEo?pid=Api&rs=1"
+        title="Sam"
+        profileSrc="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+      />
+      <Story
+        image="https://th.bing.com/th/id/OIP.aJ1uZzv6RwE-ywKw_o8wtwHaEo?pid=Api&rs=1"
+        title="Sachin"
+        profileSrc="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+      />
+    </div>
+  );
+}
+
+export default StoryReel;
