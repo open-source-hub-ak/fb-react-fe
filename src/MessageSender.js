4b4958e4f9210e11b1a2c5f516692757c0f86955
aakash <aakash>
2021-02-07 16:30:44 +0530

    Message Sender No styled

--- src/MessageSender.js
+++ src/MessageSender.js
@@ -1,10 +1,43 @@
+import { Avatar } from "@material-ui/core";
 import React from "react";
 import "./MessageSender.css";
+import VideocamIcon from "@material-ui/icons/Videocam";
+import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+
 function MessageSender() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="messageSender">
-      <div className="messageSender__top"></div>
-      <div className="messageSender__bottom"></div>
+      <div className="messageSender__top">
+        <Avatar />
+        <form action="" method="">
+          <input
+            className="messageSender__input"
+            placeholder="Whats on your mind?"
+          />
+          <input placeholder="Image URL (Optional)" />
+          <button type="submit" onClick={handleSubmit}>
+            Hidden Submit
+          </button>
+        </form>
+      </div>
+      <div className="messageSender__bottom">
+        <div className="messageSender__option">
+          <VideocamIcon style={{ color: "red" }} />
+          <h4>Live Video</h4>
+        </div>
+        <div className="messageSender__option">
+          <PhotoLibraryIcon style={{ color: "green" }} />
+          <h4>Photo/Video</h4>
+        </div>
+        <div className="messageSender__option">
+          <InsertEmoticonIcon style={{ color: "orange" }} />
+          <h4>Feeling/Activity</h4>
+        </div>
+      </div>
     </div>
   );
 }
